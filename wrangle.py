6996aa4b651fa4beb705b1e09f124568d202ebfb
Luis Martinez <luis.martinez.sat@gmail.com>
2023-12-12 16:00:18 -0600

    added zillow wrangle python module

---
+++ wrangle.py
@@ -0,0 +1,121 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import env as e
+import os
+
+
+def get_zillow():
+    # name of cached csv
+    filename = "zillow.csv"
+    # if cached data exist
+    if os.path.isfile(filename):
+        df = pd.read_csv(filename)
+    # wrangle from sql db if not cached
+    else:
+        # read sql query into df
+        # 261 is single family residential id
+        df = pd.read_sql(
+            """SELECT yearbuilt,
+                       bedroomcnt,
+                       bathroomcnt,
+                       calculatedfinishedsquarefeet,
+                       taxvaluedollarcnt,
+                       taxamount,
+                       fips
+                FROM properties_2017
+                WHERE propertylandusetypeid = 261""",
+            f"mysql+pymysql://{e.user}:{e.password}@{e.host}/zillow",
+        )
+        # cache data locally
+        df.to_csv(filename, index=False)
+    return df
+
+
+
+
+
+
+
+
+def split_data(df, target):
+    """
+    This function takes in any DataFrame and a target variable as an argument 
+    and returns train, validate, and test dataframes.
+    It returns three DataFrames with a printout of their proportion to the original DataFrame.
+    """
+    train, validate_test = train_test_split(df, train_size=0.6, random_state=123)
+    validate, test = train_test_split(validate_test, train_size=0.5, random_state=123)
+    print(f"train: {len(train)} ({round(len(train)/len(df), 2)*100}% of {len(df)})")
+    print(
+        f"validate: {len(validate)} ({round(len(validate)/len(df), 2)*100}% of {len(df)})"
+    )
+    print(f"test: {len(test)} ({round(len(test)/len(df), 2)*100}% of {len(df)})")
+
+    return train, validate, test
+
+
+
+
+
+
+
+
+def wrangle_zillow():
+    """
+    This function reads the Zillow data from a cached CSV file if it exists,
+    or from a SQL database if it doesn't exist. It then renames the columns
+    to more descriptive names.
+
+    Args:
+    - None
+
+    Returns:
+    - pandas dataframe
+    """
+    # Name of cached CSV file
+    filename = "zillow.csv"
+    # If cached data exists, read from CSV file
+    if os.path.isfile(filename):
+        df = pd.read_csv(filename)
+    # Otherwise, read from SQL database
+    else:
+        df = pd.read_sql(
+            """SELECT yearbuilt,
+                                   bedroomcnt,
+                                   bathroomcnt,
+                                   calculatedfinishedsquarefeet,
+                                   taxvaluedollarcnt,
+                                   taxamount,
+                                   fips
+                            FROM properties_2017
+                            WHERE propertylandusetypeid = 261""",  # 261 is single family residential id
+            f"mysql+pymysql://{e.user}:{e.password}@{e.host}/zillow",
+        )
+        # Cache data locally
+        df.to_csv(filename, index=False)
+    # Rename columns
+    df = df.rename(
+        columns={
+            "yearbuilt": "year",
+            "bedroomcnt": "bedroom",
+            "bathroomcnt": "bathroom",
+            "calculatedfinishedsquarefeet": "square_feet",
+            "taxvaluedollarcnt": "property_value",
+            "taxamount": "property_tax",
+            "fips": "county",
+        }
+    )
+
+    # Drop rows with missing values in specific columns 
+    df = df.dropna()
+
+    # Map county codes to county names
+    df.county = df.county.map({6037: "LA", 6059: "Orange", 6111: "Ventura"})
+
+    # Convert columns to int data type
+    df = df.astype({"year": int, "bedroom": int, "square_feet": int, "property_value": int})
+
+    return df
+
+
+
