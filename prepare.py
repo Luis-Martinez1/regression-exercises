55499a5f49f412748302918f6edcfaea4543fc10
Luis Martinez <luis.martinez.sat@gmail.com>
2023-12-16 09:12:30 -0600

    added function

--- prepare.py
+++ prepare.py
@@ -14,7 +14,7 @@ def scale_data(train,
                return_scaler=False):
     '''
     Scales the 3 data splits. 
-    Takes in train, validate, and test data splits and returns their scaled counterparts.
+    Takes in train, validate, and test data splits and returns their scaled counterparts using MinMaxScaler.
     If return_scalar is True, the scaler object will be returned as well
     '''
     # make copies of our original data
@@ -39,6 +39,35 @@ def scale_data(train,
         return scaler, train_scaled, validate_scaled, test_scaled
     else:
         return train_scaled, validate_scaled, test_scaled
+
+
+
+
+
+
+
+
+
+# Create a function to isolate the target variable from a split dataframe
+def X_y_split(train, validate, test, target):
+    '''
+    This function takes in train, validate, and test dataframes and the target variable.
+    Then it returns the X_train, y_train, X_validate, y_validate, X_test, y_test.
+    '''  
+
+    X_train = train.drop(columns= target)
+    y_train = train[target]
+
+    X_validate = validate.drop(columns= target)
+    y_validate = validate[target]
+
+    X_test = test.drop(columns= target)
+    y_test = test[target]
+    
+    return X_train, y_train, X_validate, y_validate, X_test, y_test
+
+
+
 
 
 
